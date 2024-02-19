99b7b1eab2c13e2509865be634d1fa34ca3f8e21
HnJoel <3152331418@qq.com>
2024-02-19 15:36:49 +0000

    hcode

--- app.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>so that at the name of Jesus every knee should bow, in heaven and on earth and under the earth.(Philippians2:10 ESV)</p>"
